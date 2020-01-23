d56e3d6beac4f4c45759c08e2a09881e2e290bd7
Yuichiro Iijima <jimaichiro@gmail.com>
2020-01-23 23:40:38 +0900

    表記方法の変更

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,20 @@
     <input type="text" v-model="searchText">
     <button @click="searchMovies">検索</button>
     <br>
-    <ul v-if="movies">
+    <!-- <ul v-if="movies">
       <li v-for="movie in movies.Search" :key="movie">
         {{ movie.Title }}
+        {{ movie.Year }}
+        {{ movie.Type }}
         <img width="200" :src=movie.Poster>
       </li>
-
-    </ul>
+    </ul> -->
+    <div v-for="movie in movies.Search" :class="card" :key="movie">
+      <img :src=movie.Poster :alt=movie.Title>
+      <div class="container">
+        <h4><b>{{ movie.Title }}</b></h4>
+      </div>
+    </div>
     
     <!-- <img width="200" :src=showPoster alt=""> -->
   </div>
@@ -52,4 +59,22 @@ export default {
 </script>
 
 <style lang="scss">
+#app {
+  background-color: #ededed;
+}
+
+.card {
+  box-shadow:0 4px 8px rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  width: 40%;
+}
+img {
+  width: 250px;
+}
+.container {
+  padding: 2px 16px;
+}
+.card:hover {
+  box-shadow:0 8px 16px rgba(0, 0, 0, 0.2);
+}
 </style>
